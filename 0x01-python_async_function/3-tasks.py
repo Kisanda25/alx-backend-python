7e904acbc2318520011800f3407881a833c2e7ac
EmmanuelKisanda <emmanuelkisanda7@gmail.com>
2024-03-19 00:05:50 +0000

    Pythons ASYcn

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
-"""From the previous file, import wait_n into 2-measure_runtime.py
-Create a measure_time function with integers n and max_delay as
-arguments that measures the total execution time for wait_n(n, max_delay),
-and returns total_time / n. Your function should return a float.
-Use the time module to measure an approximate elapsed time.
+"""Import wait_random from 0-basic_async_syntax.
+
+Write a function (do not create an async function, use the regular
+function syntax to do this) task_wait_random that takes an integer
+max_delay and returns a asyncio.Task.
 """
-import time
+
+
 import asyncio
-wait_n = __import__('1-concurrent_coroutines').wait_n
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    """Returns total execution time"""
-    start_time = time.time()
-    asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-
-    total_time = end_time - start_time
-    return (total_time/n)
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Returns a asyncio.Task"""
+    return asyncio.create_task(wait_random(max_delay))
